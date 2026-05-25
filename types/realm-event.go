51e91093d2a9876476382321d7e2c13d451d3e6c
daniel <danielmarcos2@yahoo.com.br>
2026-05-25 12:57:11 -0300

    firstVersion chore: add types and utils to retrieve realm event mapping from DB

---
+++ types/realm-event.go
@@ -0,0 +1,43 @@
+package types
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type RealmEvent struct {
+	Realm              string                 `json:"realm" bson:"realm"`
+	RealmEvent         string                 `json:"realmEvent" bson:"realmEvent"`
+	RealmID            string                 `json:"realmId" bson:"realmId"`
+	RealmDate          time.Time              `json:"realmDate" bson:"realmDate"`
+	Count              int32                  `json:"count" bson:"count"`
+	Customer           []string               `json:"customer" bson:"customer"`
+	TpCustomer         []string               `json:"tpCustomer" bson:"tpCustomer"`
+	Metric             []map[string]float64   `json:"metric" bson:"metric"`
+	Value              []map[string]float64   `json:"value" bson:"value"`
+	Variables          map[string]interface{} `json:"variables"`
+	Modifier           []map[string]string    `json:"modifier" bson:"modifier"`
+	Service            []string               `json:"service" bson:"service"`
+	GroupBy            []string               `json:"groupBy" bson:"groupBy"`
+	BillingScope       []string               `json:"billingScope" bson:"billingScope"`
+	Info               []map[string]string    `json:"info" bson:"info"`
+	AccountID          bson.ObjectID          `json:"account,omitempty" bson:"account"`
+	ThirdPartAccountID bson.ObjectID          `json:"tpAccount,omitempty" bson:"tpAccount"`
+	TpName             string                 `json:"tpName,omitempty" bson:"tpName"`
+	MetaData           MetaData               `json:"metadata" bson:"metadata"`
+	EventSource        string                 `json:"eventSource" bson:"eventSource"`
+	Raw                string                 `json:"raw" bson:"raw"`
+}
+
+type MetaData struct {
+	AccountingId  string    `json:"accountingId" bson:"accountingId"`
+	RatingId      string    `json:"ratingId" bson:"ratingId"`
+	EventId       string    `json:"eventId" bson:"eventId"`
+	Channel       string    `json:"channel" bson:"channel"`
+	PipelineStage string    `json:"pipelineStage" bson:"pipelineStage"`
+	Status        string    `json:"status" bson:"status"`
+	StatusCode    string    `json:"statusCode" bson:"statusCode"`
+	Version       int32     `json:"version" bson:"version"`
+	Timestamp     time.Time `json:"timestamp" bson:"timestamp"`
+}
---
+++ types/record-map.go
@@ -0,0 +1,44 @@
+package types
+
+import "time"
+
+type RecordMap struct {
+	Realm           string
+	Event           string
+	Location        *time.Location
+	SplitEventField string
+	Id              []RecordField
+	IdIndex         []RecordField
+	Date            RecordField
+	Count           RecordField
+	CountIndex      int
+	TpName          string
+	Customer        []RecordField
+	TPCustomer      []RecordField
+	Modifiers       []RecordField
+	Info            []RecordField
+	Service         []RecordField
+	Metrics         []RecordField
+	Values          []RecordField
+	Variables       []RecordField
+	GroupBy         []RecordField
+	BillingScope    []RecordField
+	FieldFilters    []FieldFilter
+}
+
+type RecordField struct {
+	Index      int
+	Name       string
+	RemoteName string
+	Format     string
+	Value      string
+	DataType   string
+	Unique     bool
+}
+
+type FieldFilter struct {
+	Index    int
+	Path     string
+	Criteria string
+	Values   map[string]bool
+}
